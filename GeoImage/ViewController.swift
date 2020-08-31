37dc4025a93ec11eb75c83fb3a56f4d436f235e4
Stanislav <staspovolotskiy@gmail.com>
2020-08-31 16:32:34 +0300

    - getting location for previously saved photos - showing in imageView photos from gallery and camera

--- GeoImage/ViewController.swift
+++ GeoImage/ViewController.swift
@@ -7,14 +7,73 @@
 //
 
 import UIKit
+import Photos
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+
+    var imagePicker: ImagePicker!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        self.imagePicker = ImagePicker(presentationController: self, delegate: self)
     }
 
-
+    @IBAction func showImagePicker(_ sender: UIButton) {
+        self.imagePicker.present(from: sender)
+    }
+    
+//    func addAsset(image: UIImage, location: CLLocation? = nil) {
+//           PHPhotoLibrary.shared().performChanges({
+//               // Request creating an asset from the image.
+//               let creationRequest = PHAssetChangeRequest.creationRequestForAsset(from: image)
+//               // Set metadata location
+//               if let location = location {
+//                   creationRequest.location = location
+//               }
+//           }, completionHandler: { success, error in
+//               if !success { NSLog("error creating asset: \(error)") }
+//           })
+//    }
 }
 
+extension ViewController: ImagePickerDelegate {
+
+    func didSelect(image: UIImage?) {
+        self.imageView.image = image
+        
+        
+//        var locationManager = CLLocationManager()
+//        locationManager.requestWhenInUseAuthorization()
+//        var currentLoc: CLLocation!
+//        if(CLLocationManager.authorizationStatus() == .authorizedWhenInUse ||
+//        CLLocationManager.authorizationStatus() == .authorizedAlways) {
+//           currentLoc = locationManager.location
+//           print(currentLoc.coordinate.latitude)
+//           print(currentLoc.coordinate.longitude)
+//        }
+//
+//        addAsset(image: image!, location: currentLoc)
+//        print("BLAA: \(image?.getExifData())")
+    }
+}
+
+extension UIImage {
+
+    func getExifData() -> CFDictionary? {
+        var exifData: CFDictionary? = nil
+        if let data = self.jpegData(compressionQuality: 1.0) {
+            data.withUnsafeBytes {
+                let bytes = $0.baseAddress?.assumingMemoryBound(to: UInt8.self)
+                if let cfData = CFDataCreate(kCFAllocatorDefault, bytes, data.count),
+                    let source = CGImageSourceCreateWithData(cfData, nil) {
+                    exifData = CGImageSourceCopyPropertiesAtIndex(source, 0, nil)
+                }
+            }
+        }
+        return exifData
+    }
+}
