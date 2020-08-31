67d2e1234e3ff6009aa655087f5ef82506b0fe1a
Stanislav <staspovolotskiy@gmail.com>
2020-08-31 08:41:10 +0300

    Initial Commit

---
+++ GeoImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeoImage
+//
+//  Created by Stanislav Povolotskiy on 31.08.2020.
+//  Copyright © 2020 Stanislav Povolotskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
